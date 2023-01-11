631727e480a95303bfd05bbde8f0b2615063047e
ahmedatefnagy <ahmedatefnagy10@gmail.com>
2023-01-11 08:46:01 +0200

    test1

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const menuButton = document.querySelector('.menu-button');
-const navList = document.querySelector('.nav-list');
-
-if(menuButton){
-  menuButton.addEventListener('click', ()=>{
-    navList.classList.toggle('open');
-  })
-}
